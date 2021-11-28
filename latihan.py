9e227c0a62d9c8a178620c5cb56de777abfaa9c7
Ridwanwildan <rdwnwldn@gmail.com>
2021-11-28 22:32:58 +0700

    add code

---
+++ latihan.py
@@ -0,0 +1,29 @@
+# Ada list yang memiliki 5 elemen
+a = [10, 20, 30, 40, 50]
+
+# Tampilkan elemen ke 3
+print(a[2])
+
+# Ambil nilai elemen ke 2 sampai ke 4
+a[1:4]
+
+# Ambil elemen terakhir
+a[4]
+
+# Ubah elemen ke 4 dengan nilai lainnya
+a[3] = 4444
+
+# Ubah elemen ke 4 sampai dengan elemen terakhir
+a[3:] = [3000, 4000]
+
+# Ambil 2 bagian dari list pertama (a) dan jadikan list kedua (b)
+b = a[0:2]
+
+# Tambah list B dengan nilai string
+b.append("Hizbullah")
+
+# Tambah list B dengan 3 nilai
+b.extend([66, 77, 88])
+
+# Gabungkan list B dengan list A
+c = a + b
---
+++ praktikum4.py
@@ -0,0 +1,29 @@
+nama = []
+nim = []
+tugas = []
+uts = []
+uas = []
+ulang = "y"
+
+while ulang == "y":
+ nama.append(input("Nama : "))
+ nim.append(input("NIM : "))
+ tugas.append(int(input("Nilai tugas : ")))
+ uts.append(int(input("Nilai UTS : ")))
+ uas.append(int(input("Nilai UAS : ")))
+ ulang = input("Tambah data (y/t) ? ")
+
+print("==========================================================================")
+print("| No  |          Nama           |    NIM    | Tugas | UTS | UAS |  Akhir |")
+print("==========================================================================")
+
+for i in range(len(nama)):
+ print("|", i+1, "  |", end="")
+ print('{0:<25}'.format(nama[i]), end="")
+ print("|", nim[i], end="")
+ print(" |", tugas[i], end="")
+ print("    |", uts[i], end="")
+ print("  |", uas[i], " | ", end="")
+ print(f'{(tugas[i] + uts[i] + uas[i]) / 3:.2f}', " |")
+print("==========================================================================")
+
